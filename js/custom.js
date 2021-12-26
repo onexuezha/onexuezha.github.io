aca18e5fdc360e4cfecae9ed023bb2bc6b2e6de0
onexuezha <2324920675@qq.com>
2021-12-26 11:37:02 +0800

    Site updated: 2021-12-26 11:37:01

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,45 @@
-"use strict";function categoriesBarActive(){var e,t=window.location.pathname,t=decodeURIComponent(t);console.log(t),"/"==t?document.querySelector("#category-bar")&&document.getElementById("首页").classList.add("select"):(e=/\/categories\/.*?\//.test(t),console.log(e),e&&(t=t.split("/"),console.log(t[2]),t=t[2],document.querySelector("#category-bar")&&document.getElementById(t).classList.add("select")))}function topCategoriesBarScroll(){var o;document.getElementById("category-bar-items")&&(o=document.getElementById("category-bar-items")).addEventListener("mousewheel",function(e){var t=-e.wheelDelta/2;o.scrollLeft+=t,e.preventDefault()},!1)}categoriesBarActive(),topCategoriesBarScroll();
+categoriesBarActive()
+topCategoriesBarScroll()
+
+//分类条
+function categoriesBarActive(){
+  var urlinfo = window.location.pathname;
+  urlinfo = decodeURIComponent(urlinfo)
+  console.log(urlinfo);
+  //判断是否是首页
+  if (urlinfo == '/'){
+    if (document.querySelector('#category-bar')){
+      document.getElementById('首页').classList.add("select")
+    }
+  }else {
+    // 验证是否是分类链接
+    var pattern = /\/categories\/.*?\//;
+    var patbool = pattern.test(urlinfo);
+    console.log(patbool);
+    // 获取当前的分类
+    if (patbool) {
+      var valuegroup = urlinfo.split("/");
+      console.log(valuegroup[2]);
+      // 获取当前分类
+      var nowCategorie = valuegroup[2];
+      if (document.querySelector('#category-bar')){
+        document.getElementById(nowCategorie).classList.add("select");
+      }
+    }
+  }
+  
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll(){
+  if (document.getElementById("category-bar-items")){
+    let xscroll = document.getElementById("category-bar-items");
+  xscroll.addEventListener("mousewheel", function (e) {
+    //计算鼠标滚轮滚动的距离
+    let v = -e.wheelDelta / 2;
+    xscroll.scrollLeft += v;
+    //阻止浏览器默认方法
+    e.preventDefault();
+}, false);
+  }
+}
